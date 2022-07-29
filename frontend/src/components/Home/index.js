2222f5a93f5eac4f22ff38b5f2641af551a31375
Martin Schimandl <martin.schimandl@gmail.com>
2022-07-29 18:00:47 +0100

    works almost

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -45,7 +45,7 @@ class Home extends React.Component {
   render() {
     return (
       <div className="home-page">
-        <Banner />
+        <Banner items={this.props.items} />
 
         <div className="container page">
           <Tags tags={this.props.tags} onClickTag={this.props.onClickTag} />
