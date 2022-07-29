a1b973e1952678245e91a4f8ff547607e17c18c2
Martin Schimandl <martin.schimandl@gmail.com>
2022-07-29 18:02:01 +0100

    revert irrelvant thing

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -45,7 +45,7 @@ class Home extends React.Component {
   render() {
     return (
       <div className="home-page">
-        <Banner items={this.props.items} />
+        <Banner />
 
         <div className="container page">
           <Tags tags={this.props.tags} onClickTag={this.props.onClickTag} />
