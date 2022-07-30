54611154562e954b1c30240f22b06dc805a9935a
Martin Schimandl <martin.schimandl@gmail.com>
2022-07-30 09:02:07 +0100

    reset title state at some more events

--- frontend/src/reducers/itemList.js
+++ frontend/src/reducers/itemList.js
@@ -55,6 +55,7 @@ const reducer = (state = {}, action) => {
         tab: null,
         tag: action.tag,
         currentPage: 0,
+        title: null,
       };
     case HOME_PAGE_LOADED:
       return {
@@ -77,6 +78,7 @@ const reducer = (state = {}, action) => {
         tab: action.tab,
         currentPage: 0,
         tag: null,
+        title: null,
       };
     case PROFILE_PAGE_LOADED:
     case PROFILE_FAVORITES_PAGE_LOADED:
