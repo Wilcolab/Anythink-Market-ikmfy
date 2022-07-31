1b08ec4248d6aa03320fae1337b16f0d91dc1440
Martin Schimandl <martin.schimandl@gmail.com>
2022-07-31 09:25:03 +0100

    make the searchbar magically appear

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -14,8 +14,9 @@ const mapDispatchToProps = (dispatch) => ({
 class Banner extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { title: "" };
+    this.state = { title: "", searchbar_visible: "" };
     this.handleChange = this.handleChange.bind(this);
+    this.handleOnClick = this.handleOnClick.bind(this);
   }
 
   handleChange(event) {
@@ -32,19 +33,28 @@ class Banner extends React.Component {
     }
   }
 
+  handleOnClick(event) {
+    event.preventDefault();
+    this.setState({ searchbar_visible: "bla" });
+  }
+
   render() {
     return (
       <div className="banner text-white">
         <div className="container p-4 text-center">
           <img src={logo} alt="banner" />
           <div>
-            <span id="get-part">A place to get</span>
-            <input
-              id="search-box"
-              type="text"
-              value={this.state.title}
-              onChange={this.handleChange}
-            />
+            <span id="get-part" onClick={this.handleOnClick}>
+              A place to get
+            </span>
+            {this.state.searchbar_visible ? (
+              <input
+                id="search-box"
+                type="text"
+                value={this.state.title}
+                onChange={this.handleChange}
+              />
+            ) : null}
             <span> the cool stuff.</span>
           </div>
         </div>
