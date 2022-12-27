72b8b6a93370119bc4f35a265faab760210340e7
Martin Schimandl <martin.schimandl@gmail.com>
2022-12-27 15:31:48 +0000

    Add new field is_verfied to user table

---
+++ backend/app/db/migrations/versions/5b5befff79f7_add_is_verified_field_to_user_table.py
@@ -0,0 +1,21 @@
+"""Add is_verified field to user table
+
+Revision ID: 5b5befff79f7
+Revises: fdf8821871d7
+Create Date: 2022-12-27 15:28:57.260629
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = '5b5befff79f7'
+down_revision = 'fdf8821871d7'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    op.add_column('users', sa.Column('is_verfied', sa.Boolean))
+
+def downgrade():
+    op.drop_column('users', 'is_verified')
